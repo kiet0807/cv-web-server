d2168f0cad5af4f7e2e5edbfeb8423a58330e1e8
kiet0807 <kiet080797@gmail.com>
2021-06-11 13:53:08 +0700

    ADD

---
+++ models/account.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/my_database", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const Schema = mongoose.Schema;
+
+const AccountSchema = new Schema(
+  {
+    username: String,
+    password: String,
+  },
+  {
+    collection: "account",
+  }
+);
+
+const AccountModel = mongoose.model("account", AccountSchema);
+
+module.exports = AccountModel;
---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require("express");
+var app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const AccountModel = require("./models/account");
+
+app.post("/register", (req, res, next) => {
+  var username = req.body.username;
+  var password = req.body.password;
+
+  AccountModel.findOne({
+    username: username,
+    password: password,
+  })
+    .then((data) => {
+      if (data) {
+        res.json("User này đã tồn tại");
+      } else {
+        return AccountModel.create({
+          username: username,
+          password: password,
+        });
+      }
+    })
+    .then((data) => {
+      res.json("Tao tai khoan thanh cong");
+    })
+    .catch((err) => {
+      res.status(500).json("Tao tai khoan that bai");
+    });
+});
+
+app.post("/login", (req, res, next) => {
+  var username = req.body.username;
+  var password = req.body.password;
+
+  AccountModel.findOne({
+    username: username,
+    password: password,
+  })
+    .then((data) => {
+      if (data) {
+        res.json("Đăng nhập thành công");
+      } else {
+        res.status(400).json("Đăng nhập thất bại");
+      }
+    })
+    .catch((err) => {
+      res.status(500).json("Có lỗi bên server");
+    });
+});
+
+app.get("/", (req, res, next) => {
+  res.json("Home");
+});
+
+app.listen(3000, () => {
+  console.log(`Server started on port`);
+});
