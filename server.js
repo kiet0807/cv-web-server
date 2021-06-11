d4d9962d5a71f0d3ba8405d632dd396207c41aba
kiet0807 <kiet080797@gmail.com>
2021-06-11 15:24:06 +0700

    fix

--- server.js
+++ server.js
@@ -59,6 +59,6 @@ app.get("/home", (req, res, next) => {
   res.json("home");
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`Server started on port`);
 });
