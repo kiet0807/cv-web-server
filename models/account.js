9b488f783bdf753da46c141b44704f0cbbe2b68b
kiet0807 <kiet080797@gmail.com>
2021-06-12 16:08:08 +0700

    fix

--- models/account.js
+++ models/account.js
@@ -1,8 +1,18 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/my_database", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+
+const mongoAtlasUri =
+  "mongodb+srv://kiet87:kiet0347551122@cluster0.nltqm.mongodb.net/account?retryWrites=true&w=majority";
+
+try {
+  // Connect to the MongoDB cluster
+  mongoose.connect(
+    mongoAtlasUri,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => console.log(" Mongoose blog mindx is connected")
+  );
+} catch (e) {
+  console.log("could not connect");
+}
 
 const Schema = mongoose.Schema;
 
