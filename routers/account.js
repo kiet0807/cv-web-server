e634577e0ddac212c2c2d053632e5a4f2c3e38eb
kiet0807 <kiet080797@gmail.com>
2021-06-12 18:01:10 +0700

    fix

--- routers/account.js
+++ routers/account.js
@@ -10,7 +10,7 @@ router.get("/", (req, res, next) => {
       res.json(data);
     })
     .catch((err) => {
-      res.status(500).json("Lỗi server: ", err);
+      res.status(500).json(err);
     });
 });
 
