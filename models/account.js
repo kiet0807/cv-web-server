e259db3ea63c7df00cce3b30a28dc33d244340cd
kiet0807 <kiet080797@gmail.com>
2021-06-16 15:20:17 +0700

    fix

--- models/account.js
+++ models/account.js
@@ -1,17 +1,13 @@
 const mongoose = require("mongoose");
 
-const MONGODB_URI = "mongodb://localhost/my_database";
+const MONGODB_URI =
+  "mongodb+srv://kiet87:kiet0347551122@cluster0.nltqm.mongodb.net/account?retryWrites=true&w=majority";
 
 try {
   // Connect to the MongoDB cluster
   mongoose.connect(
-    MONGODB_URI,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-    },
+    process.env.MONGODB_URI,
+    { useNewUrlParser: true, useUnifiedTopology: true },
     () => console.log("Mongoose đã kết nối")
   );
 } catch (e) {
